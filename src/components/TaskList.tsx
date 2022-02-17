7437fa50e1c9061939ab04bd482d4847ac1c8e8d
danielvitor2d <ripsea.rpg321@gmail.com>
2022-02-17 12:14:46 -0300

    Finish Desafio 01 - Conceitos do React

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.length !== 0) {
+      const newTast: Task = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+
+      const newSetTasks = [...tasks, newTast]
+      setTasks(newSetTasks)
+
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newSetTasks = tasks.map(task => {
+      task.id === id && (task.isComplete = (task.isComplete ? false : true))
+      return task
+    })
+    setTasks(newSetTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newSetTasks = tasks.filter(task => {
+      return task.id !== id && (true)
+    })
+    setTasks(newSetTasks)
   }
 
   return (
